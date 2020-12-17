127b7657d63eb39b971e45f77388eab0c3ffa894
thatdevhassan <thatdevhassan@gmail.com>
2020-12-16 19:44:30 -0500

    fixing bugs and refactoring

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ class Rot13:
     A basic implementation of how you can
     encrypt your data using rot 13 algo
     """
-    def __init__(self,inp):
+    def __init__(self):
         print("type '-help' for help")
-        self.inp = inp
+        
 
-    def help_func(self):
+    def help_func(self,inp):
+        self.inp = inp
         print('''
         Welcome to the rot 13 world:
         here are some of the commands that you can use
@@ -18,7 +19,8 @@ class Rot13:
 
 
 
-    def cvt_to_rot(self):
+    def cvt_to_rot(self, inp):
+        self.inp = inp
         """
           Convert a string to rot13
         """
@@ -26,7 +28,8 @@ class Rot13:
             if type(i) == str:
                 print(chr(int(ord(i))+13),end='')
 
-    def cvt_to_string(self):
+    def cvt_to_string(self,inp):
+        self.inp = inp
         """
         convert a rot13 to string
 
@@ -36,18 +39,22 @@ class Rot13:
                 print(chr(int(ord(i))-13),end='')
 
 
-try:
-    while True:
-        inp = input()
-        if type(inp) == str:
-                rt = Rot13(inp)
-                if inp == '-help':
-                    rt.help_func()
-                elif inp == '-ctr':
-                    rt.cvt_to_rot()
-                elif inp == "-cts":
-                    rt.cvt_to_string()
+# try:
+#     while True:
+#         enter_cmd = input(" ")
+#         if type(enter_cmd) == str:
+#                 # rt = Rot13(enter_cmd)
+#                 if enter_cmd == '-help':
+#                     rt.help_func()
+#                 elif enter_cmd == '-ctr':
+#                     rt.cvt_to_rot()
+#                 elif enter_cmd == "-cts":
+#                     rt.cvt_to_string()
 
 
-except:
-    print("the input must be a string")
+# except:
+#     print("the input must be a string")
+rt = Rot13()
+rt.cvt_to_rot("hello my name is hassan")
+print("\n")
+rt.cvt_to_string("hello my")
